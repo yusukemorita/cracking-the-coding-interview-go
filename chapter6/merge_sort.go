f7ce40befa75d5dd458e1e822140268167640def
yusukemorita <yusuke.ppq@gmail.com>
2024-02-06 16:16:21 +0100

    add explanation about time complexity

--- chapter6/merge_sort.go
+++ chapter6/merge_sort.go
@@ -1,13 +1,28 @@
 package main
 
-// time complexity: O(N^2)
-// space complexity: O(1)
+// time complexity: O(NlogN)
+//   - each merge takes x operations, where x is the
+//     sum of the length of the two merged slices
+//   - since `mergeSort` is a recursive function, and in each recursion
+//     the slices are halved, we can think of the structure of the recursion
+//     like a binary tree
+//   - at each level of the binary tree, the total number of items merged together
+//     is always N. At the top level merge, two slices of length N/2 are merged,
+//     and at the bottom level, N separate slices of length 1 are merged into N/2 slices.
+//   - therefore, the time complexity is
+//     (depth of binary tree) * (length of items in the original slice)
+//   - since the depth of the binary tree will correlate to logN, this means the time complexity is
+//     O(logN * N)
+//   - ref: https://www.youtube.com/watch?v=MBxqi6GrfN8
+// space complexity: O(N)
+//   - for the helper array, which has a length of N
 func MergeSort(slice []int) {
-	mergeSort(slice, 0, len(slice))
+	helper := make([]int, len(slice))
+	mergeSort(slice, helper, 0, len(slice))
 }
 
 // startIndex is inclusive, endIndex is exclusive
-func mergeSort(slice []int, startIndex, endIndex int) {
+func mergeSort(slice, helper []int, startIndex, endIndex int) {
 	if startIndex+1 >= endIndex {
 		// section has length of 1 or less, nothing to sort
 		return
@@ -16,16 +31,21 @@ func mergeSort(slice []int, startIndex, endIndex int) {
 	// length is larger than 1
 	newSectionLength := (endIndex - startIndex) / 2
 	middleIndex := startIndex + newSectionLength
-	mergeSort(slice, startIndex, middleIndex)
-	mergeSort(slice, middleIndex, endIndex)
-	merge(slice, startIndex, middleIndex, endIndex)
+	mergeSort(slice, helper, startIndex, middleIndex)
+	mergeSort(slice, helper, middleIndex, endIndex)
+	merge(slice, helper, startIndex, middleIndex, endIndex)
 }
 
-func merge(slice []int, startIndex, middleIndex, endIndex int) {
+func merge(slice, helper []int, startIndex, middleIndex, endIndex int) {
 	slice1Index := startIndex
 	slice2Index := middleIndex
-	result := make([]int, endIndex-startIndex)
-	resultIndex := 0
+
+	// copy items in both sections into the helper
+	for i := startIndex; i < endIndex; i++ {
+		helper[i] = slice[i]
+	}
+	// to track where to insert the next item
+	sliceIndex := startIndex
 
 	for {
 		completedSlice1 := slice1Index >= middleIndex
@@ -36,34 +56,29 @@ func merge(slice []int, startIndex, middleIndex, endIndex int) {
 		}
 
 		if completedSlice1 {
-			result[resultIndex] = slice[slice2Index]
-			resultIndex++
+			slice[sliceIndex] = helper[slice2Index]
+			sliceIndex++
 			slice2Index++
 			continue
 		}
 
 		if completedSlice2 {
-			result[resultIndex] = slice[slice1Index]
-			resultIndex++
+			slice[sliceIndex] = helper[slice1Index]
+			sliceIndex++
 			slice1Index++
 			continue
 		}
 
-		item1 := slice[slice1Index]
-		item2 := slice[slice2Index]
+		item1 := helper[slice1Index]
+		item2 := helper[slice2Index]
 
 		if item1 <= item2 {
-			result[resultIndex] = slice[slice1Index]
-			resultIndex++
+			slice[sliceIndex] = item1
 			slice1Index++
 		} else {
-			result[resultIndex] = slice[slice2Index]
-			resultIndex++
+			slice[sliceIndex] = item2
 			slice2Index++
 		}
-	}
-
-	for i, item := range result {
-		slice[i+startIndex] = item
+		sliceIndex++
 	}
 }
